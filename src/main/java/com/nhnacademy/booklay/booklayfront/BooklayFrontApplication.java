6d4473d9365e679f31966c31768c797e6d1e865d
yangrystal <tmddk512@naver.com>
2023-01-02 22:15:12 +0900

    Initial commit

---
+++ src/main/java/com/nhnacademy/booklay/booklayfront/BooklayFrontApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.booklay.booklayfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooklayFrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooklayFrontApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhnacademy/booklay/booklayfront/BooklayFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.booklay.booklayfront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooklayFrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
