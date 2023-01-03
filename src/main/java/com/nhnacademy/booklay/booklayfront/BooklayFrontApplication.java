f0864a19456445aef4a72c16c7b313b495e82c55
Com-Sun <gnlslfflffl@gmail.com>
2023-01-03 17:26:38 +0900

    feat: 깃플로우 새로 생성#36

--- src/main/java/com/nhnacademy/booklay/booklayfront/BooklayFrontApplication.java
+++ src/main/java/com/nhnacademy/booklay/booklayfront/BooklayFrontApplication.java
@@ -8,6 +8,7 @@ public class BooklayFrontApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BooklayFrontApplication.class, args);
+        //test gitflow
     }
 
 }
