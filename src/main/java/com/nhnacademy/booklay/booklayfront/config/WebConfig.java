5ecc3596ef3801a3727999fc16ad5a44619d8ee4
Com-Sun <gnlslfflffl@gmail.com>
2023-01-08 21:53:41 +0900

    feat: gateway ip를 외부로부터 받아오게 변경

---
+++ src/main/java/com/nhnacademy/booklay/booklayfront/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.nhnacademy.booklay.booklayfront.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public String gatewayIp(@Value("${booklay.gateway-origin}") String ip) {
+
+        return ip;
+    }
+}
--- src/main/java/com/nhnacademy/booklay/booklayfront/controller/IndexController.java
+++ src/main/java/com/nhnacademy/booklay/booklayfront/controller/IndexController.java
@@ -1,19 +1,28 @@
 package com.nhnacademy.booklay.booklayfront.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
+@RequiredArgsConstructor
 public class IndexController {
+
+    private final RestTemplate restTemplate;
+    private final String gatewayIp;
 
     @GetMapping("/index")
     public String index() {
         return "index";
     }
 
+    @ResponseBody
     @GetMapping("/shop")
     public String shop() {
-        return "shop";
+        String testMember = restTemplate.getForObject( gatewayIp+"/shop/v1/members", String.class);
+        return testMember;
     }
 
     @GetMapping("/detail")
