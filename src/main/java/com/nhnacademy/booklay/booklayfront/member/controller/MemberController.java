6d0c3cef89eec2e01753b28189ffd94e8ceb964e
yangrystal <tmddk512@naver.com>
2023-01-13 09:56:06 +0900

    fix: MemberController의 회원가입 부분 로직 수정

--- src/main/java/com/nhnacademy/booklay/booklayfront/member/controller/MemberController.java
+++ src/main/java/com/nhnacademy/booklay/booklayfront/member/controller/MemberController.java
@@ -1,22 +1,18 @@
 package com.nhnacademy.booklay.booklayfront.member.controller;
 
-import static javax.swing.text.html.CSS.getAttribute;
-
 import com.nhnacademy.booklay.booklayfront.coupon.domain.FrontURI;
 import com.nhnacademy.booklay.booklayfront.member.domain.MemberCreateRequest;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -27,14 +23,31 @@ public class MemberController {
     private final RestTemplate restTemplate;
     private final FrontURI frontURI;
 
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public String test() {
+        return "member/createMemberForm";
+    }
+
     @PostMapping
     public String postCreateCouponType(@ModelAttribute MemberCreateRequest memberCreateRequest,
                                        RedirectAttributes redirectAttributes) {
         HttpEntity<MemberCreateRequest> request = new HttpEntity<>(memberCreateRequest);
-        ResponseEntity<Void> response = restTemplate.exchange(frontURI.SHOPURI + "/members", HttpMethod.POST, request, Void.class);
+
+        /**
+         * localhost로 돌아가는 코드
+         * 후에 삭제
+         */
+//        ResponseEntity<Void> response =
+//            restTemplate.exchange("http://localhost:9090" + "/members", HttpMethod.POST, request,
+//                Void.class);
+
+        ResponseEntity<Void> response =
+            restTemplate.exchange(frontURI.SHOPURI + "/members", HttpMethod.POST, request,
+                Void.class);
 
         //TODO 1: 에러처리
-        if(!response.getStatusCode().equals(HttpStatus.CREATED)) {
+        if (!response.getStatusCode().equals(HttpStatus.CREATED)) {
             redirectAttributes.addFlashAttribute("error", response.getStatusCode());
             return "redirect:/error";
         }
