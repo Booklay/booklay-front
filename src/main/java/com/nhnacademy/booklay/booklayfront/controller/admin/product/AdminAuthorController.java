4ec42c026580a63ffcd2a5d7a52a83fd0e65c8a2
최규태 <kuitae0725@gmail.com>
2023-01-13 10:28:12 +0900

    feat : product 프론트 작업 #7

--- src/main/java/com/nhnacademy/booklay/booklayfront/controller/admin/product/AdminAuthorController.java
+++ src/main/java/com/nhnacademy/booklay/booklayfront/controller/admin/product/AdminAuthorController.java
@@ -1,5 +1,70 @@
 package com.nhnacademy.booklay.booklayfront.controller.admin.product;
 
+import com.nhnacademy.booklay.booklayfront.dto.PageResponse;
+import com.nhnacademy.booklay.booklayfront.dto.product.tag.response.RetrieveTagResponse;
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/admin/product/tag/maintenance")
 public class AdminAuthorController {
 
+  private final RestTemplate restTemplate;
+  private final String gatewayIp;
+
+
+  @GetMapping()
+  public String retrieveTag(
+      @RequestParam(value = "page", required = false) Optional<Integer> pageNum, Model model) {
+    if (pageNum.isEmpty()) {
+      pageNum = Optional.of(0);
+    }
+    if (pageNum.get() < 0) {
+      pageNum = Optional.of(0);
+    }
+
+    Long size = 20L;
+
+    pageNum = Optional.of(pageNum.get() - 1);
+
+    HttpHeaders httpHeaders = new HttpHeaders();
+    httpHeaders.setAccept(List.of(MediaType.APPLICATION_JSON));
+
+    URI uri = URI.create(
+        gatewayIp + "/shop/v1/admin/product/author?page=" + pageNum.get() + "&size=" + size);
+
+    RequestEntity<PageResponse<RetrieveTagResponse>> requestEntity = new RequestEntity<>(
+        httpHeaders, HttpMethod.GET, uri);
+
+    ResponseEntity<PageResponse<RetrieveTagResponse>> testTags =
+        restTemplate.exchange(requestEntity, new ParameterizedTypeReference<>() {
+        });
+
+    int totalPage = testTags.getBody().getTotalPages();
+    int nowPage = testTags.getBody().getPageNumber();
+    List<RetrieveTagResponse> tagList = testTags.getBody().getData();
+
+    model.addAttribute("nowPage", nowPage);
+    model.addAttribute("totalPage", totalPage);
+    model.addAttribute("tagList", tagList);
+
+    return "/admin/product/adminTag";
+  }
 }
