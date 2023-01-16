a90e051830fb50223c28b90f93a01ce7e22ea84f
최규태 <kuitae0725@gmail.com>
2023-01-17 00:50:06 +0900

    feat : 상품 등록 폼 개발#0116

--- src/main/java/com/nhnacademy/booklay/booklayfront/controller/admin/product/ProductController.java
+++ src/main/java/com/nhnacademy/booklay/booklayfront/controller/admin/product/ProductController.java
@@ -9,11 +9,13 @@ import java.net.URI;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,6 +24,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
+
 
 @Slf4j
 @Controller
@@ -41,6 +44,56 @@ public class ProductController {
   public String getProductBookForm() {
     return "/admin/product/createProductBookForm";
   }
+
+//  @PostMapping("/book/create")
+//  public String createProductBook(@Valid @ModelAttribute CreateProductBookRequest request)
+//      throws IOException {
+//    log.info("제목 : " + request.getTitle());
+//    log.info("ISBN : " + request.getIsbn());
+//    log.info("긴설명 : " + request.getLongDescription());
+//    log.info("짧설명 : " + request.getShortDescription());
+//    log.info("페이지 : " + request.getPage());
+//    log.info("작가리스트 : " + request.getAuthorIds().get(0));
+//    log.info(
+//        "사진 :" + request.getImage().getOriginalFilename() + request.getImage().getContentType());
+//
+//    URI uri = URI.create(gatewayIp + "/shop/v1/admin/product/register/book");
+//
+//    ObjectMapper mapper = new ObjectMapper().registerModule(new JavaTimeModule());
+//
+//    CreateProductBookJson jsonString = new CreateProductBookJson(request);
+//
+//    MultipartFile image = request.getImage();
+//
+//    LinkedMultiValueMap<String, String> headerMap = new LinkedMultiValueMap<>();
+//    headerMap.add("Content-disposition",
+//        "form-data; name=image; filename=" + image.getOriginalFilename());
+//    headerMap.add("Content-type", "application/octet-stream");
+//    HttpEntity<byte[]> imageBytes = new HttpEntity<>(image.getBytes(), headerMap);
+//
+//    LinkedMultiValueMap<String, Object> multipartReqMap = new LinkedMultiValueMap<>();
+//    multipartReqMap.add("jsonString", mapper.writeValueAsString(jsonString));
+////    multipartReqMap.add("jsonString", jsonString);
+//    multipartReqMap.add("image", imageBytes);
+//
+//    HttpHeaders headers = new HttpHeaders();
+//    headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+//
+//    RequestEntity<LinkedMultiValueMap<String, Object>> requestEntity = new RequestEntity<>(
+//        multipartReqMap, headers, HttpMethod.POST, uri);
+//
+//    log.info("출력 : " + requestEntity.getBody().get("jsonString"));
+//    log.info("출력 : " + requestEntity.getBody().get("image"));
+//
+//    ResponseEntity<Long> responseEntity =restTemplate.exchange(requestEntity, Long.class);
+//
+////    HttpEntity<?> uploadEntity = new HttpEntity<>(multipartReqMap, headers);
+////    restTemplate.exchange(uri, HttpMethod.POST, uploadEntity, String.class);
+//
+//    Long productId = responseEntity.getBody();
+//    log.info("생성된 상품 번호 : " + productId);
+//    return "redirect:/admin/product";
+//  }
 
   @PostMapping("/book/create")
   public String createProductBook(@Valid @ModelAttribute CreateProductBookRequest request)
@@ -62,21 +115,20 @@ public class ProductController {
 
     MultipartFile image = request.getImage();
 
-    LinkedMultiValueMap<String, String> headerMap = new LinkedMultiValueMap<>();
-    headerMap.add("Content-disposition",
-        "form-data; name=image; filename=" + image.getOriginalFilename());
-    headerMap.add("Content-type", "application/octet-stream");
-    HttpEntity<byte[]> imageBytes = new HttpEntity<>(image.getBytes(), headerMap);
+    ByteArrayResource contentsAsResource = new ByteArrayResource(image.getBytes()){
+      @Override
+      public String getFilename(){
+        return image.getOriginalFilename();
+      }
+    };
 
     LinkedMultiValueMap<String, Object> multipartReqMap = new LinkedMultiValueMap<>();
     multipartReqMap.add("jsonString", mapper.writeValueAsString(jsonString));
 //    multipartReqMap.add("jsonString", jsonString);
-    multipartReqMap.add("image", imageBytes);
+    multipartReqMap.add("image", contentsAsResource);
 
     HttpHeaders headers = new HttpHeaders();
     headers.setContentType(MediaType.MULTIPART_FORM_DATA);
-
-    HttpEntity<?> uploadEntity = new HttpEntity<>(multipartReqMap, headers);
 
     RequestEntity<LinkedMultiValueMap<String, Object>> requestEntity = new RequestEntity<>(
         multipartReqMap, headers, HttpMethod.POST, uri);
@@ -84,11 +136,13 @@ public class ProductController {
     log.info("출력 : " + requestEntity.getBody().get("jsonString"));
     log.info("출력 : " + requestEntity.getBody().get("image"));
 
-//    ResponseEntity<Long> responseEntity =restTemplate.exchange(requestEntity, Long.class);
-    restTemplate.exchange(uri, HttpMethod.POST, uploadEntity, String.class);
+    ResponseEntity<Long> responseEntity =restTemplate.exchange(requestEntity, Long.class);
 
-//    Long productId = responseEntity.getBody();
-//    log.info("생성된 상품 번호 : " + productId);
+//    HttpEntity<?> uploadEntity = new HttpEntity<>(multipartReqMap, headers);
+//    restTemplate.exchange(uri, HttpMethod.POST, uploadEntity, String.class);
+
+    Long productId = responseEntity.getBody();
+    log.info("생성된 상품 번호 : " + productId);
     return "redirect:/admin/product";
   }
 
