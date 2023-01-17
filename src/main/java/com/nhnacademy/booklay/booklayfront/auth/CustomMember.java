b5dde4f0f31693da0df10a22d372201ed95ef92d
Com-Sun <gnlslfflffl@gmail.com>
2023-01-17 14:49:56 +0900

    feat: CustomUserDetails 및 Authentication Provider 구현

---
+++ src/main/java/com/nhnacademy/booklay/booklayfront/auth/CustomMember.java
@@ -0,0 +1,53 @@
+package com.nhnacademy.booklay.booklayfront.auth;
+
+import com.nhnacademy.booklay.booklayauth.constant.Roles;
+import com.nhnacademy.booklay.booklayfront.constant.Roles;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class CustomMember implements UserDetails {
+
+    private String username;
+
+    private String password;
+
+    private List<Roles> authorities;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities.stream()
+                .map(m -> new SimpleGrantedAuthority(m.getValue()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
