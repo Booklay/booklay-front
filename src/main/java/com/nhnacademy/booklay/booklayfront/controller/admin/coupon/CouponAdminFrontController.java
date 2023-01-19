00bc1aaba1965d9cebb8359bfeb06e6474abfd9c
opgumo <opgumo@naver.com>
2023-01-19 16:47:49 +0900

    fix: SonarLint codesmell 제거

--- src/main/java/com/nhnacademy/booklay/booklayfront/controller/admin/coupon/CouponAdminFrontController.java
+++ src/main/java/com/nhnacademy/booklay/booklayfront/controller/admin/coupon/CouponAdminFrontController.java
@@ -50,13 +50,16 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("/admin/coupon")
 public class CouponAdminFrontController {
     private final RestService restService;
-    private final String gatewayIp;
+    private final ObjectMapper objectMapper;
     private final ImageUploader imageUploader;
+    private final String gatewayIp;
+
     private static final String RETURN_PAGE = "admin/adminPage";
     private static final String RETURN_PAGE_COUPON_LIST = "redirect:/admin/coupon/list/0";
+
     private static final String REST_PREFIX = "/coupon/v1";
-    private static final String URL_PREFIX = "/admin/coupons";
-    private final ObjectMapper objectMapper;
+    private static final String COUPON_URL_PREFIX = "/admin/coupons";
+    private static final String COUPON_TYPES_URL_PREFIX = "/admin/couponTypes";
 
     @ModelAttribute("navHead")
     public String addNavHead() {
@@ -71,7 +74,7 @@ public class CouponAdminFrontController {
 
     @GetMapping("create")
     public String createCouponForm(Model model) {
-        String url = buildString(gatewayIp, REST_PREFIX, "/admin/couponTypes");
+        String url = buildString(gatewayIp, REST_PREFIX, COUPON_TYPES_URL_PREFIX);
 
         ApiEntity<PageResponse<CouponType>>
             apiEntity = restService.get(url, getDefaultPageMap(0),
@@ -95,18 +98,20 @@ public class CouponAdminFrontController {
         @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm") Date date,
         @RequestParam(name = "couponImage", required = false) MultipartFile multipartFile,
         HttpServletRequest request) {
+
         couponAddRequest.setIssuanceDeadlineAt(
             date.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime());
         String imagePath = imageUploader.uploadImage(multipartFile, request);
         Map<String, Object> map = objectMapper.convertValue(couponAddRequest, Map.class);
-
-        // FIXME Image 저장 후, 반환 값
         map.put("imageId", 1L);
-        String url = buildString(gatewayIp, REST_PREFIX, URL_PREFIX);
+
+        String url = buildString(gatewayIp, REST_PREFIX, COUPON_URL_PREFIX);
+
         ApiEntity<String> apiEntity = restService.post(url, map, String.class);
         if (!apiEntity.isSuccess()) {
             return ERROR;
         }
+
         return RETURN_PAGE_COUPON_LIST;
     }
 
@@ -119,9 +124,11 @@ public class CouponAdminFrontController {
     @PostMapping("type/create")
     public String postCreateCoupon(@ModelAttribute("CouponTypeAddRequest")
                                    CouponTypeAddRequest couponTypeAddRequest) {
+        String url = buildString(gatewayIp, REST_PREFIX, COUPON_TYPES_URL_PREFIX);
+
         Map<String, Object> map = objectMapper.convertValue(couponTypeAddRequest, Map.class);
-        String url = buildString(gatewayIp, REST_PREFIX, "/admin/couponTypes");
         restService.post(url, map, String.class);
+
         return "redirect:/admin/coupon/list/type/0";
     }
 
@@ -132,15 +139,16 @@ public class CouponAdminFrontController {
 
     @GetMapping("list/{pageNum}")
     public String allCouponList(Model model, @PathVariable Integer pageNum) {
-        String url = buildString(gatewayIp, REST_PREFIX, URL_PREFIX, "/pages");
+        String url = buildString(gatewayIp, REST_PREFIX, COUPON_URL_PREFIX, "/pages");
+
         ApiEntity<PageResponse<Coupon>> apiEntity =
             restService.get(url, getDefaultPageMap(pageNum), new ParameterizedTypeReference<>() {
             });
+
         if (!apiEntity.isSuccess()) {
             return ERROR;
         }
 
-        // FIXME : Boolean is null.
         model.addAttribute(ATTRIBUTE_NAME_COUPON_LIST, apiEntity.getBody().getData());
         model.addAttribute(ATTRIBUTE_NAME_MEMBER_NO, "");
         model.addAttribute(PAGE_NUM, pageNum);
@@ -151,7 +159,7 @@ public class CouponAdminFrontController {
     @GetMapping("list/type/{pageNum}")
     public String allCouponTypeList(Model model, @PathVariable Integer pageNum) {
 
-        String url = buildString(gatewayIp, REST_PREFIX, "/admin/couponTypes");
+        String url = buildString(gatewayIp, REST_PREFIX, COUPON_TYPES_URL_PREFIX);
         ApiEntity<PageResponse<CouponType>> apiEntity =
             restService.get(url, getDefaultPageMap(pageNum), new ParameterizedTypeReference<>() {
             });
@@ -166,7 +174,7 @@ public class CouponAdminFrontController {
 
     @GetMapping("type/delete/{couponId}")
     public String couponTypeDelete(@PathVariable String couponId) {
-        String url = buildString(gatewayIp, REST_PREFIX, "/admin/couponTypes/", couponId);
+        String url = buildString(gatewayIp, REST_PREFIX, COUPON_TYPES_URL_PREFIX, "/", couponId);
         restService.delete(url);
         return "redirect:/admin/coupon/list/type/0";
     }
@@ -174,7 +182,6 @@ public class CouponAdminFrontController {
     @GetMapping("list/{memberNo}/{pageNum}")
     public String memberCouponList(Model model, @PathVariable String memberNo,
                                    @PathVariable Integer pageNum) {
-        //todo
         String url =
             buildString(gatewayIp, REST_PREFIX, "/members/", memberNo, "/", pageNum.toString());
         ApiEntity<PageResponse<Coupon>> apiEntity =
@@ -205,7 +212,7 @@ public class CouponAdminFrontController {
 
     @GetMapping("update/{couponId}")
     public String updateCouponForm(Model model, @PathVariable String couponId) {
-        String url = buildString(gatewayIp, REST_PREFIX, URL_PREFIX, "/", couponId);
+        String url = buildString(gatewayIp, REST_PREFIX, COUPON_URL_PREFIX, "/", couponId);
         ApiEntity<CouponDetail> apiEntity = restService.get(url, null, CouponDetail.class);
         if (!apiEntity.isSuccess()) {
             return ERROR;
@@ -231,7 +238,7 @@ public class CouponAdminFrontController {
 
     @GetMapping("delete/{couponId}")
     public String deleteCoupon(@PathVariable String couponId) {
-        String url = buildString(gatewayIp, REST_PREFIX, URL_PREFIX, "/", couponId);
+        String url = buildString(gatewayIp, REST_PREFIX, COUPON_URL_PREFIX, "/", couponId);
         restService.delete(url);
         return RETURN_PAGE_COUPON_LIST;
     }
@@ -296,9 +303,8 @@ public class CouponAdminFrontController {
 
     @GetMapping("/issue")
     public String issueCouponForm(Model model) {
-        String url = buildString(gatewayIp, REST_PREFIX, URL_PREFIX, "/pages");
+        String url = buildString(gatewayIp, REST_PREFIX, COUPON_URL_PREFIX, "/pages");
 
-        // 쿠폰 모든 종류 찾아서 받아오고, 팝업창에서 선택에서 선택할 수 있게끔..
         ApiEntity<PageResponse<Coupon>> apiEntity =
             restService.get(url, getDefaultPageMap(0), new ParameterizedTypeReference<>() {
             });
@@ -328,6 +334,9 @@ public class CouponAdminFrontController {
 
     @GetMapping("/member/issue")
     public String issueCouponToMemberForm(Model model) {
+
+        model.addAttribute(TARGET_VIEW, "coupon/issueCouponToMemberForm");
+
         return RETURN_PAGE;
     }
 
@@ -338,7 +347,7 @@ public class CouponAdminFrontController {
 
     @GetMapping("/popup/pages/{pageNum}")
     public String couponPopup(@PathVariable int pageNum, Model model) {
-        String url = buildString(gatewayIp, REST_PREFIX, URL_PREFIX, "/pages");
+        String url = buildString(gatewayIp, REST_PREFIX, COUPON_URL_PREFIX, "/pages");
         ApiEntity<PageResponse<Coupon>> apiEntity =
             restService.get(url, getDefaultPageMap(pageNum), new ParameterizedTypeReference<>() {
             });
@@ -347,6 +356,22 @@ public class CouponAdminFrontController {
         }
 
         model.addAttribute("couponList", apiEntity.getBody().getData());
+
+        return "/admin/coupon/couponPopup";
+    }
+
+    @GetMapping("/member/popup/pages/{pageNum}")
+    public String memberPopup(@PathVariable int pageNum, Model model) {
+        String url = buildString(gatewayIp, "/shop/v1/admin/members");
+        ApiEntity<PageResponse<Coupon>> apiEntity =
+            restService.get(url, getDefaultPageMap(pageNum), new ParameterizedTypeReference<>() {
+            });
+
+        if (!apiEntity.isSuccess()) {
+            return ERROR;
+        }
+
+        model.addAttribute("memberList", apiEntity.getBody().getData());
 
         return "/admin/coupon/couponPopup";
     }
--- src/main/resources/static/js/coupon/couponPopup.js
+++ src/main/resources/static/js/coupon/couponPopup.js
@@ -8,3 +8,13 @@ function setParentText(value){
     opener.document.getElementById("couponId").value = value;
     window.close();
 }
+
+function showMemberPopup() {
+    let option = "width = 700, height = 500, top = 100, left = 200, scrollbars = yes, location = no"
+    let ret = window.open("/admin/coupon/member/popup/pages/0", "memberPopup", option);
+}
+
+function setMemberText(value){
+    opener.document.getElementById("couponId").value = value;
+    window.close();
+}
