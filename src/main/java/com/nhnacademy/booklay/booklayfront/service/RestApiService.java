9ac462c7ad090254950687562e2611925b182874
오준후 <ojh789ojh@naver.com>
2023-01-20 10:10:15 +0900

    쿠폰 front html오타 수정

--- src/main/java/com/nhnacademy/booklay/booklayfront/service/RestApiService.java
+++ src/main/java/com/nhnacademy/booklay/booklayfront/service/RestApiService.java
@@ -6,7 +6,9 @@ import com.nhnacademy.booklay.booklayfront.dto.coupon.PageResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
 
+import static com.nhnacademy.booklay.booklayfront.dto.coupon.ControllerStrings.ATTRIBUTE_NAME_COUPON_TYPE_LIST;
 import static com.nhnacademy.booklay.booklayfront.utils.ControllerUtil.buildString;
 import static com.nhnacademy.booklay.booklayfront.utils.ControllerUtil.getDefaultPageMap;
 
@@ -19,9 +21,10 @@ public class RestApiService {
     private static final String RETURN_PAGE_COUPON_LIST = "redirect:/admin/coupon/list/0";
     private static final String REST_PREFIX = "/coupon/v1";
     private static final String URL_PREFIX = "/admin/coupons";
-    public ApiEntity<PageResponse<CouponType>> getAllCouponType(){
+    public void setAllCouponTypeToModel(Model model){
         String url = buildString(gatewayIp, REST_PREFIX, "/admin/couponTypes");
-        return restService.get(url, getDefaultPageMap(0),
-                new ParameterizedTypeReference<>() {});
+        ApiEntity<PageResponse<CouponType>> apiEntity =
+                restService.get(url, getDefaultPageMap(0), new ParameterizedTypeReference<>() {});
+        model.addAttribute(ATTRIBUTE_NAME_COUPON_TYPE_LIST, apiEntity.getBody().getData());
     }
 }
