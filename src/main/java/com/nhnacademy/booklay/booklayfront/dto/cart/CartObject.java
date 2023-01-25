ad72898a4a92eb34cd1fb84fe421888802ee1dbd
joonhoo oh <ojh789ojh@naver.com>
2023-01-25 11:03:35 +0900

    cartListForm 제작중

---
+++ src/main/java/com/nhnacademy/booklay/booklayfront/dto/cart/CartObject.java
@@ -0,0 +1,15 @@
+package com.nhnacademy.booklay.booklayfront.dto.cart;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CartObject {
+    private Long productNo;
+    private String productName;
+    private String productPrice;
+    private Integer productCount;
+}
