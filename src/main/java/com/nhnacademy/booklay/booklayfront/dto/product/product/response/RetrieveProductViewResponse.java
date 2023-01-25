856b733e5184543c60bd1c8533f9c58d0e7d99d7
최규태 <kuitae0725@gmail.com>
2023-01-25 10:24:11 +0900

    feat : 상품 상세조회 화면 개발 #1

---
+++ src/main/java/com/nhnacademy/booklay/booklayfront/dto/product/product/response/RetrieveProductViewResponse.java
@@ -0,0 +1,68 @@
+package com.nhnacademy.booklay.booklayfront.dto.product.product.response;
+
+import com.nhnacademy.booklay.booklayfront.dto.product.author.response.RetrieveAuthorResponse;
+import com.nhnacademy.booklay.booklayfront.dto.product.tag.response.RetrieveTagResponse;
+import java.time.LocalDate;
+import java.util.List;
+import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.multipart.MultipartFile;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RetrieveProductViewResponse {
+
+  @NotNull
+  private Long productId;
+  @Setter
+  private MultipartFile image;
+  @NotNull
+  private String title;
+  @NotNull
+  private Long price;
+  @NotNull
+  private Long pointRate;
+  @NotNull
+  private String shortDescription;
+  @NotNull
+  private String longDescription;
+  @NotNull
+  private boolean isSelling;
+  @NotNull
+  private boolean pointMethod;
+
+  private List<RetrieveTagResponse> productTags;
+
+  //책 상세
+  private Long productDetailId;
+  private String isbn;
+  private int page;
+  @DateTimeFormat(pattern = "yyyy-MM-dd")
+  private LocalDate publishedDate;
+  @Setter
+  private String ebookAddress;
+  @Setter
+  private int storage;
+  private List<RetrieveAuthorResponse> authors;
+  private List<Long> categoryIds;
+
+  //구독 상품 상세
+  private Long subscribeId;
+  @Length(max = 4)
+  private Long subscribeWeek;
+  @Length(max = 7)
+  private Long subscribeDay;
+  private List<Long> childProducts;
+
+  //책 구독 상품 상세 공통
+  @NotNull
+  private String publisher;
+}
+
+
