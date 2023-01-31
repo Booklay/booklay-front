76bfa273394657035e2743f50a8acf44c0fbdfa5
yangrystal <tmddk512@naver.com>
2023-01-31 17:28:35 +0900

    feat: 회원 탈퇴 구현, 개인정보 수정 구현

--- src/main/java/com/nhnacademy/booklay/booklayfront/controller/member/MemberController.java
+++ src/main/java/com/nhnacademy/booklay/booklayfront/controller/member/MemberController.java
@@ -4,10 +4,12 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nhnacademy.booklay.booklayfront.dto.coupon.ApiEntity;
 import com.nhnacademy.booklay.booklayfront.dto.member.request.MemberCreateRequest;
+import com.nhnacademy.booklay.booklayfront.dto.member.request.MemberUpdateRequest;
 import com.nhnacademy.booklay.booklayfront.dto.member.response.MemberRetrieveResponse;
 import com.nhnacademy.booklay.booklayfront.service.RestService;
 import java.net.URI;
 import java.util.List;
+import java.util.Map;
 import javax.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
@@ -19,7 +21,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -35,24 +36,18 @@ public class MemberController {
     private final RestTemplate restTemplate;
     private final String redirectGatewayPrefix;
     private final RestService restService;
+    private final ObjectMapper objectMapper;
+
     private final static String MYPAGE = "/mypage/myPage";
 
-    public MemberController(RestTemplate restTemplate, RestService restService, String gateway) {
+    public MemberController(RestTemplate restTemplate, RestService restService, String gateway,
+                            ObjectMapper objectMapper) {
         this.restTemplate = restTemplate;
         this.restService = restService;
+        this.objectMapper = objectMapper;
         redirectGatewayPrefix = gateway + "/shop/v1" + "/members";
     }
 
-    @GetMapping("/login")
-    public String retrieveLoginForm() {
-        return "member/loginForm";
-    }
-
-    @GetMapping("/register")
-    @ResponseStatus(HttpStatus.OK)
-    public String retrieveCreateMemberForm(Model model) {
-        return "member/createMemberForm";
-    }
 
     @PostMapping("/register")
     public String createMember(@Valid @ModelAttribute MemberCreateRequest memberCreateRequest,
@@ -76,6 +71,17 @@ public class MemberController {
         return "redirect:/";
     }
 
+    @GetMapping("/login")
+    public String retrieveLoginForm() {
+        return "member/loginForm";
+    }
+
+    @GetMapping("/register")
+    @ResponseStatus(HttpStatus.OK)
+    public String retrieveCreateMemberForm(Model model) {
+        return "member/createMemberForm";
+    }
+
     @GetMapping("/{memberNo}")
     public String retrieveMemberDetail(@PathVariable Long memberNo, Model model) {
         HttpHeaders httpHeaders = new HttpHeaders();
@@ -93,12 +99,51 @@ public class MemberController {
         model.addAttribute("memberNo", memberNo);
         model.addAttribute("targetUrl", "member/memberDetail");
 
-        return "mypage/member/memberDetail";
+        return MYPAGE;
     }
 
-    @GetMapping("/delete/{memberNo}")
-    public String deleteMember(@PathVariable Long memberNo, Model model) {
-        URI uri = URI.create(redirectGatewayPrefix + "/delete/" + memberNo);
+    @GetMapping("/update/{memberNo}")
+    public String retrieveMemberUpdateForm(@PathVariable Long memberNo, Model model) {
+        String query = "/" + memberNo;
+
+        URI uri = URI.create(redirectGatewayPrefix + query);
+
+        ApiEntity<MemberRetrieveResponse> response =
+            restService.get(uri.toString(), null, MemberRetrieveResponse.class);
+
+        if (response.isSuccess()) {
+            model.addAttribute("member", response.getBody());
+            model.addAttribute("memberNo", memberNo);
+
+            return "mypage/member/memberUpdateForm";
+        } else {
+            return "/";
+        }
+    }
+
+    @PostMapping("/update/{memberNo}")
+    public String updateMember(@PathVariable Long memberNo,
+                               @Valid @ModelAttribute MemberUpdateRequest request,
+                               BindingResult bindingResult) {
+
+        URI uri = URI.create(redirectGatewayPrefix + "/" + memberNo);
+
+        restService.put(uri.toString(), objectMapper.convertValue(request, Map.class),
+            Void.class);
+
+        return "redirect:/members/" + memberNo;
+    }
+
+    @GetMapping("/drop/{memberNo}")
+    public String retrieveDeleteMemberForm(@PathVariable Long memberNo, Model model) {
+        model.addAttribute("memberNo", memberNo);
+
+        return "mypage/member/memberDeleteForm";
+    }
+
+    @GetMapping("/drop/process/{memberNo}")
+    public String deleteMember(@PathVariable Long memberNo) {
+        URI uri = URI.create(redirectGatewayPrefix + "/" + memberNo);
 
         restService.delete(uri.toString(), null);
 
