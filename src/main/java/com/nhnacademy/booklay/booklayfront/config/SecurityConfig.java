a0a3add9868043c6d5952d469b413f7c25db0d7f
Com-Sun <gnlslfflffl@gmail.com>
2023-01-31 15:39:44 +0900

    feat: 로그아웃 오류 해결
    스프링 시큐리티의 기본 logout() 설정

--- src/main/java/com/nhnacademy/booklay/booklayfront/config/SecurityConfig.java
+++ src/main/java/com/nhnacademy/booklay/booklayfront/config/SecurityConfig.java
@@ -32,15 +32,17 @@ public class SecurityConfig {
     @Bean
     protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http.formLogin().disable()
-                .logout().logoutUrl("/members/logout")
-                .logoutSuccessUrl("/");
+            .logout().logoutUrl("/members/logout")
+            .deleteCookies("SESSION_ID")
+            .invalidateHttpSession(true)
+            .logoutSuccessUrl("/");
 
         http.authorizeRequests()
-                .anyRequest()
-                .permitAll();
+            .anyRequest()
+            .permitAll();
 
         http.csrf()
-                .disable();
+            .disable();
 
         http.addFilterAt(getInitialAuthenticationFilter(), BasicAuthenticationFilter.class);
 
@@ -52,12 +54,12 @@ public class SecurityConfig {
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() throws Exception {
         return web -> web.ignoring()
-                .antMatchers("/resources/**", "/static/**", "/webjars/**", "/img/**");
+                         .antMatchers("/resources/**", "/static/**", "/webjars/**", "/img/**");
     }
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration)
-            throws Exception {
+        throws Exception {
         return configuration.getAuthenticationManager();
     }
 
