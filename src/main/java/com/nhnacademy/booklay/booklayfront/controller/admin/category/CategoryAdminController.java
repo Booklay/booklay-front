20604d1892bfb145e43bf130999f255cf17c65dc
최규태 <kuitae0725@gmail.com>
2023-02-03 19:54:14 +0900

    refact : Model and View -> Model

--- src/main/java/com/nhnacademy/booklay/booklayfront/controller/admin/category/CategoryAdminController.java
+++ src/main/java/com/nhnacademy/booklay/booklayfront/controller/admin/category/CategoryAdminController.java
@@ -22,156 +22,150 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 @Controller
 @RequestMapping("/admin/categories")
 public class CategoryAdminController {
-    private final String redirectGatewayPrefix;
-    private final RestService restService;
-    private final ObjectMapper objectMapper;
 
-    public CategoryAdminController(RestService restService,
-                                   String gateway, ObjectMapper objectMapper) {
-        this.redirectGatewayPrefix = gateway + "/shop/v1" + "/admin/categories";
-        this.restService = restService;
-        this.objectMapper = objectMapper;
+  private final String redirectGatewayPrefix;
+  private final RestService restService;
+  private final ObjectMapper objectMapper;
+
+  public CategoryAdminController(RestService restService,
+      String gateway, ObjectMapper objectMapper) {
+    this.redirectGatewayPrefix = gateway + "/shop/v1" + "/admin/categories";
+    this.restService = restService;
+    this.objectMapper = objectMapper;
+  }
+
+  private static final String REDIRECT_PREFIX = "redirect:/admin/categories";
+
+  /**
+   * 카테고리 등록.
+   *
+   * @param createRequest .
+   * @param model         .
+   * @return .
+   */
+  @PostMapping
+  public String createCategory(@Valid @ModelAttribute CategoryCreateRequest createRequest,
+      Model model) {
+
+    URI uri = URI.create(redirectGatewayPrefix);
+
+    ApiEntity<CategoryResponse> response =
+        restService.post(uri.toString(), objectMapper.convertValue(createRequest, Map.class),
+            CategoryResponse.class);
+
+    model.addAttribute("category", response.getBody());
+
+    return REDIRECT_PREFIX;
+  }
+
+  /**
+   * 카테고리 등록 화면.
+   *
+   * @return .
+   */
+  @GetMapping("/create")
+  public String createForm() {
+    return "admin/category/createForm";
+  }
+
+  /**
+   * 카테고리 수정 화면.
+   *
+   * @param id    .
+   * @param model .
+   * @return .
+   */
+  @GetMapping("/{categoryId}/update")
+  public String updateForm(@PathVariable(name = "categoryId") Long id, Model model) {
+    URI uri = URI.create(redirectGatewayPrefix + "/" + id);
+
+    ApiEntity<CategoryResponse> categoryResponse =
+        restService.get(uri.toString(), null, CategoryResponse.class);
+
+    if (categoryResponse.isSuccess()) {
+      model.addAttribute("category", categoryResponse.getBody());
+      return "admin/category/updateForm";
+    } else {
+      return REDIRECT_PREFIX;
+    }
+  }
+
+
+  /**
+   * 카테고리 리스트 화면.
+   *
+   * @param page .
+   * @param mav  .
+   * @return .
+   */
+  @GetMapping
+  public String retrieveCategoryList(@RequestParam(value = "page", defaultValue = "0") int page,
+      Model model) {
+
+    String query = "?page=" + page;
+
+    URI uri = URI.create(redirectGatewayPrefix + query);
+
+    ApiEntity<PageResponse<CategoryResponse>> response =
+        restService.get(uri.toString(), null, new ParameterizedTypeReference<>() {
+        });
+
+    if (response.isSuccess()) {
+      model.addAttribute("list", response.getBody().getData());
+      model.addAttribute("totalPage", response.getBody().getTotalPages());
+      model.addAttribute("currentPage", response.getBody().getPageNumber());
+      return "admin/category/listView";
+    } else {
+      return "/index";
+    }
+  }
+
+  /**
+   * 카테고리 수정.
+   *
+   * @param request .
+   * @param id      .
+   * @return .
+   */
+  @PutMapping("/{categoryId}")
+  public String updateCategory(@Valid @ModelAttribute CategoryUpdateRequest request,
+      @PathVariable("categoryId") Long id) {
+
+    URI uri = URI.create(redirectGatewayPrefix + "/" + id);
+
+    ApiEntity<CategoryResponse> categoryResponse =
+        restService.put(uri.toString(), objectMapper.convertValue(request, Map.class),
+            CategoryResponse.class);
+
+    if (categoryResponse.isSuccess()) {
+      return REDIRECT_PREFIX;
     }
 
-    private static final String REDIRECT_PREFIX = "redirect:/admin/categories";
-
-    /**
-     * 카테고리 등록.
-     *
-     * @param createRequest .
-     * @param model         .
-     * @return .
-     */
-    @PostMapping
-    public String createCategory(@Valid @ModelAttribute CategoryCreateRequest createRequest,
-                                 Model model) {
-
-        URI uri = URI.create(redirectGatewayPrefix);
-
-        ApiEntity<CategoryResponse> response =
-            restService.post(uri.toString(), objectMapper.convertValue(createRequest, Map.class),
-                CategoryResponse.class);
-
-        model.addAttribute("category", response.getBody());
+    return REDIRECT_PREFIX + "/" + id + "/update";
+  }
 
 
-        return REDIRECT_PREFIX;
-    }
+  /**
+   * 카테고리 삭제.
+   *
+   * @param id .
+   * @return .
+   */
+  @DeleteMapping("/{categoryId}")
+  public String deleteCategory(@PathVariable("categoryId") Long id,
+      @RequestParam(value = "page", defaultValue = "0") int page) {
 
-    /**
-     * 카테고리 등록 화면.
-     *
-     * @return .
-     */
-    @GetMapping("/create")
-    public String createForm() {
-        return "admin/category/createForm";
-    }
+    String query = "?page=" + page;
 
-    /**
-     * 카테고리 수정 화면.
-     *
-     * @param id  .
-     * @param mav .
-     * @return .
-     */
-    @GetMapping("/{categoryId}/update")
-    public ModelAndView updateForm(@PathVariable(name = "categoryId") Long id, ModelAndView mav) {
-        URI uri = URI.create(redirectGatewayPrefix + "/" + id);
+    URI uri = URI.create(redirectGatewayPrefix + "/" + id);
 
-        ApiEntity<CategoryResponse> categoryResponse =
-            restService.get(uri.toString(), null, CategoryResponse.class);
+    restService.delete(uri.toString());
 
-        if (categoryResponse.isSuccess()) {
-            mav.addObject("category", categoryResponse.getBody());
-            mav.setViewName("admin/category/updateForm");
-        } else {
-            mav.setViewName(REDIRECT_PREFIX);
-        }
-
-        return mav;
-    }
-
-
-    /**
-     * 카테고리 리스트 화면.
-     *
-     * @param page .
-     * @param mav  .
-     * @return .
-     */
-    @GetMapping
-    public ModelAndView retrieveCategoryList(
-        @RequestParam(value = "page", defaultValue = "0") int page,
-        ModelAndView mav) {
-
-        String query = "?page=" + page;
-
-        URI uri = URI.create(redirectGatewayPrefix + query);
-
-        ApiEntity<PageResponse<CategoryResponse>> response =
-            restService.get(uri.toString(), null, new ParameterizedTypeReference<>() {
-            });
-
-        if (response.isSuccess()) {
-            mav.addObject("list", response.getBody().getData());
-            mav.addObject("totalPage", response.getBody().getTotalPages());
-            mav.addObject("currentPage", response.getBody().getPageNumber());
-            mav.setViewName("admin/category/listView");
-        } else {
-            mav.setViewName("/index");
-        }
-
-        return mav;
-    }
-
-    /**
-     * 카테고리 수정.
-     *
-     * @param request .
-     * @param id      .
-     * @return .
-     */
-    @PutMapping("/{categoryId}")
-    public String updateCategory(@Valid @ModelAttribute CategoryUpdateRequest request,
-                                 @PathVariable("categoryId") Long id) {
-
-        URI uri = URI.create(redirectGatewayPrefix + "/" + id);
-
-        ApiEntity<CategoryResponse> categoryResponse =
-            restService.put(uri.toString(), objectMapper.convertValue(request, Map.class),
-                CategoryResponse.class);
-
-        if (categoryResponse.isSuccess()) {
-            return REDIRECT_PREFIX;
-        }
-
-        return REDIRECT_PREFIX + "/" + id + "/update";
-    }
-
-
-    /**
-     * 카테고리 삭제.
-     *
-     * @param id .
-     * @return .
-     */
-    @DeleteMapping("/{categoryId}")
-    public String deleteCategory(@PathVariable("categoryId") Long id,
-                                 @RequestParam(value = "page", defaultValue = "0") int page) {
-
-        String query = "?page=" + page;
-
-        URI uri = URI.create(redirectGatewayPrefix + "/" + id);
-
-        restService.delete(uri.toString());
-
-        return REDIRECT_PREFIX + query;
-    }
+    return REDIRECT_PREFIX + query;
+  }
 }
