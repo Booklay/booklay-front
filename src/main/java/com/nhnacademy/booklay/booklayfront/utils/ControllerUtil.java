75907253f1ea4eefe460a7b409691f41c8ac11fb
오준후 <ojh789ojh@naver.com>
2023-02-03 15:39:26 +0900

    ControllerUtil 수정

--- src/main/java/com/nhnacademy/booklay/booklayfront/utils/ControllerUtil.java
+++ src/main/java/com/nhnacademy/booklay/booklayfront/utils/ControllerUtil.java
@@ -12,7 +12,6 @@ import org.springframework.util.MultiValueMap;
 import java.util.HashMap;
 import java.util.Map;
 
-//@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 @Component
 @SuppressWarnings("unchecked")
 public class ControllerUtil {
