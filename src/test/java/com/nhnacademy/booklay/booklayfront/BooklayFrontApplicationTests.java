f457ab0888bc8618706a92bb57129eb461af2d66
Com-Sun <gnlslfflffl@gmail.com>
2023-02-04 17:23:34 +0900

    fix: conflict

--- src/test/java/com/nhnacademy/booklay/booklayfront/BooklayFrontApplicationTests.java
+++ src/test/java/com/nhnacademy/booklay/booklayfront/BooklayFrontApplicationTests.java
@@ -2,8 +2,10 @@ package com.nhnacademy.booklay.booklayfront;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("prod")
 class BooklayFrontApplicationTests {
 
     @Test
