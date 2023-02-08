129ee96b9f0091bb910047e4393c7a736cc4a9e4
최규태 <kuitae0725@gmail.com>
2023-02-08 16:30:44 +0900

    feat : 바로구매, 카트 버튼 추가

--- src/main/java/com/nhnacademy/booklay/booklayfront/dto/product/RetrieveByIdRequest.java
+++ src/main/java/com/nhnacademy/booklay/booklayfront/dto/product/RetrieveByIdRequest.java
@@ -1,13 +1,13 @@
 package com.nhnacademy.booklay.booklayfront.dto.product;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class RetrieveByIdRequest {
 
   Long id;
-
-  public RetrieveByIdRequest(Long id) {
-    this.id = id;
-  }
 }
