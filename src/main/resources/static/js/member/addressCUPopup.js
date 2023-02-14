d744e91e6c118bf94c81d683b601fe5466d2ebad
yangrystal <tmddk512@naver.com>
2023-02-14 23:56:32 +0900

    fix: 배송지 수정 폼 popUp으로 작성

---
+++ src/main/resources/static/js/member/addressCUPopup.js
@@ -0,0 +1,9 @@
+function showAddressCreatePopup() {
+    let option = "width = 700, height = 750, top = 100, left = 200, scrollbars = yes, location = no"
+    let ret = window.open("/member/profile/address/register", "addressRegisterPopup", option);
+}
+
+function showAddressUpdatePopup(value) {
+    let option = "width = 700, height = 900, top = 100, left = 200, scrollbars = yes, location = no"
+    let ret = window.open("/member/profile/address/update/" + value, "addressUpdatePopup", option);
+}
