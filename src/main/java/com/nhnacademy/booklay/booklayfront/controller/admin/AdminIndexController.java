7256c96c2b654b2bf4b4a304695923c498895f63
최규태 <kuitae0725@gmail.com>
2023-02-19 15:56:09 +0900

    feat : 관리자 게시판 메인에 최근 공지사항 5개 출력

--- src/main/java/com/nhnacademy/booklay/booklayfront/controller/admin/AdminIndexController.java
+++ src/main/java/com/nhnacademy/booklay/booklayfront/controller/admin/AdminIndexController.java
@@ -10,19 +10,21 @@ import com.nhnacademy.booklay.booklayfront.service.RestService;
 import java.net.URI;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/admin")
 public class AdminIndexController extends BaseController {
 
   private final String gatewayIp;
-  private final String SHOP_PRE_FIX;
+  private static final String SHOP_PRE_FIX = "/shop/v1/";
   private final RestService restService;
   private static final Integer MAIN_MAX_POST_NUM = 5;
 
@@ -30,6 +32,8 @@ public class AdminIndexController extends BaseController {
   public String getAdminPage(Model model) {
 
     URI uri = URI.create(gatewayIp + SHOP_PRE_FIX + "/board/notice/" + MAIN_MAX_POST_NUM);
+
+    log.info(uri.toString());
 
     ApiEntity<List<PostResponse>> postResponse = restService.get(uri.toString(), null,
         new ParameterizedTypeReference<>() {
