b0d0b3285dc9882993e3b11f923adad13e283106
yangrystal <tmddk512@naver.com>
2023-02-19 19:27:48 +0900

    feat: 회원가입 시 비밀번호 확인 추가

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -6,3 +6,12 @@ function showPassword() {
         x.type = "password";
     }
 }
+
+function showPasswordRegister() {
+    const x = document.getElementById("pwd");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
