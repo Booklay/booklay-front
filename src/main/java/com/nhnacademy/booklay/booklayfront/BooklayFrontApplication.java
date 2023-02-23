646af8098fb3ef99fc7a00245b60c1da7052ba94
devel0624@gmail.com <devel0624@gmail.com>
2023-02-23 16:17:48 +0900

    feature: log&crash , eureka 적용

--- src/main/java/com/nhnacademy/booklay/booklayfront/BooklayFrontApplication.java
+++ src/main/java/com/nhnacademy/booklay/booklayfront/BooklayFrontApplication.java
@@ -2,12 +2,14 @@ package com.nhnacademy.booklay.booklayfront;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
 @EnableRedisHttpSession
+@EnableDiscoveryClient
 public class BooklayFrontApplication {
 
     public static void main(String[] args) {
